dc2299d34661c4e1cc558af0395bb3f25934cd31
rachitssharma <rachitsharma613@gmail.com>
2021-07-14 19:38:16 -0700

    Reorganized Code -> adjusters are finished

--- src/UserDisplay.java
+++ src/UserDisplay.java
@@ -1,8 +1,11 @@
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Scrollbar;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
@@ -16,13 +19,14 @@ public class UserDisplay extends JPanel{
 	private Button create_maze;
 	private Button save_maze;
 	private Button print_maze;
+	private Scrollbar height_slider, width_slider;
 	private UserSelect user_select;
 	private Maze current_maze;
 	
 	public void paint(Graphics g){
 		super.paintComponent(g);
 		
-		if(current_maze == null) return;
+		if(current_maze == null || current_maze.grid.grid == null) return;
 		
 		char[][] grid = current_maze.grid.grid;
 		g.setColor(Color.BLACK);
@@ -40,6 +44,11 @@ public class UserDisplay extends JPanel{
 		
 	}
 	
+	private void adjustSliders(int new_height, int new_width){
+		height_slider.setValue(new_height);
+		width_slider.setValue(new_width);
+	}
+	
 	private void createMazeComponent(){
 		create_maze = new Button("Create New");
 		
@@ -47,8 +56,11 @@ public class UserDisplay extends JPanel{
 		ComponentListener listener = new ComponentAdapter() {
 		      public void componentHidden(ComponentEvent evt) {
 		    	  int complexity = user_select.components_panel.findComplexity();
-		    	  current_maze = new Maze(10+10*complexity, 20+10*complexity);
+		    	  int new_maze_height = 10+10*complexity;
+		    	  int new_maze_width = 20+10*complexity;
+		    	  current_maze = new Maze(new_maze_height, new_maze_width);
 		    	  current_maze.generateMaze();
+		    	  adjustSliders(new_maze_height, new_maze_width);
 		    	  repaint();
 		      }
 		};
@@ -92,10 +104,45 @@ public class UserDisplay extends JPanel{
 		add(print_maze);
 	}
 	
+	private void remakeMaze(){
+		int maze_h = height_slider.getValue();
+		int maze_w = width_slider.getValue();
+		current_maze = new Maze(maze_h, maze_w);
+		current_maze.generateMaze();
+		repaint();
+	}
+	
+	private void adjustMazeComponent(){
+		height_slider = new Scrollbar(Scrollbar.HORIZONTAL);
+		width_slider = new Scrollbar(Scrollbar.HORIZONTAL);
+		
+		height_slider.addAdjustmentListener(new AdjustmentListener(){
+
+			@Override
+			public void adjustmentValueChanged(AdjustmentEvent e) {
+				remakeMaze();
+			}
+			
+		});
+		
+		width_slider.addAdjustmentListener(new AdjustmentListener(){
+
+			@Override
+			public void adjustmentValueChanged(AdjustmentEvent e) {
+				remakeMaze();
+			}
+			
+		});
+		
+		add(height_slider);
+		add(width_slider);
+	}
+	
 	private void createComponents(){
 		createMazeComponent();
 		saveMazeComponent();
 		printMazeComponent();
+		adjustMazeComponent();
 	}
 	
 	public UserDisplay() {
